b7f02bd1aa5e0b7fb500bbad8fb212b53b913dad
Brokenbass90 <brokenbass1990@gmail.com>
2021-11-05 14:29:41 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} React studies
-                <a className="grey-text text-lighten-4 right" href="#!">Repos</a>
+                <a className="grey-text text-lighten-4 right" href="https://github.com/Brokenbass90/react-store" target="_blank" rel="noreferrer">Repos</a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header(){
     return <nav className="deep-purple darken-1">
         <div className="nav-wrapper">
-            <a href="!#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repos</a></li>
+                <li><a href="https://github.com/Brokenbass90/react-store" target="_blank" rel="noreferrer">Repos</a></li>
             </ul>
         </div>
     </nav>
